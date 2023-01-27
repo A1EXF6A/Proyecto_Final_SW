bd2ee3037b28149ec25e34889d3d962858ae38f2
A1EXF6A <100436091+A1EXF6A@users.noreply.github.com>
2023-01-27 12:57:59 -0500

    PRUEBA DEL MENU

--- Proyecto_Final/src/proyecto_final/Proyecto_Final.java
+++ Proyecto_Final/src/proyecto_final/Proyecto_Final.java
@@ -4,6 +4,8 @@
  */
 package proyecto_final;
 
+import java.util.Scanner;
+
 /**
  *
  * @author HOME
@@ -15,7 +17,104 @@ public class Proyecto_Final {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        System.out.println("Hola");
+        Scanner tec = new Scanner(System.in);
+        String option;
+        boolean opc;
+        int opc6=0;
+        
+        do {
+            do {
+              System.out.println("Escoja una opcion");
+              System.out.println("1 opcion");
+              System.out.println("2 opcion");
+              System.out.println("3 opcion");
+              System.out.println("4 opcion");
+              System.out.println("Ingrese una opcion");
+              try{
+                opc6 = Integer.valueOf(tec.next());
+              }catch(NumberFormatException e){
+                  limp();
+            System.out.println("\033[41mERROR DE DATOS");
+        }
+              
+        } while (opc6<1||opc6>5);
+            switch (opc6) {
+            case 1:
+               do{            
+            
+            linea();
+            System.out.printf("\n\033[41mDEESA REPETIR LA OPCION 1 S/N: ");
+            option = tec.nextLine();
+            opc=verf(option);
+            limp();
+        }while(opc==false);
+                
+                System.out.println("\033[41mSALIO DE LA OPCION 1");
+                break;
+            case 2:
+               do{            
+            
+            linea();
+            System.out.printf("\n\033[45mDEESA REPETIR LA OPCION 2 S/N: ");
+            option = tec.nextLine();
+            opc=verf(option);
+            limp();
+        }while(opc==false);
+                System.out.println("\033[45mSALIO DE LA OPCION 2");
+               
+                break;
+            case 3:
+                do{            
+            
+            linea();
+            System.out.printf("\n\033[44mDEESA REPETIR LA OPCION 3 S/N: ");
+            option = tec.nextLine();
+            opc=verf(option);
+            limp();
+        }while(opc==false);
+                System.out.println("\033[44mSALIO DE LA OPCION 3");
+                break;
+            case 4:
+                do{            
+            
+            linea();
+            System.out.printf("\n\033[44mDEESA REPETIR LA OPCION 4 S/N: ");
+            option = tec.nextLine();
+            opc=verf(option);
+            limp();
+        }while(opc==false);
+                System.out.println("\033[44mSALIO DE LA OPCION 4");
+                break;
+            case 5:
+                limp();
+                System.out.println("\033[41mFin del programa"); 
+                System.exit(0);
+                break;
+        }
+        } while (opc6!=5);
+    }
+    public static boolean verf(String opci){
+        boolean ver = opci.matches("[sSnN]{1}");
+        if (ver==true) {
+                if(opci.equalsIgnoreCase("n")){
+                    ver=true;
+                }else{
+                    ver=false;
+                }    
+            }else{
+                System.out.println("\033[41mDATOS INCORRECTOS INGRESE S/N");
+                ver=false;
+            }
+        return ver;
+    }
+    public static void limp(){
+        for (int i = 0; i < 50; i++) {
+            System.out.println("");
+        }}
+        public static void linea(){
+        for (int i = 0; i < 198; i++) {
+            System.out.printf("-");
+        }
     }
     
 }
