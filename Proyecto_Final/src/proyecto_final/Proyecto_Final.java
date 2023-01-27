1c872a050334e5039d265806ed822468b84d57b0
A1EXF6A <100436091+A1EXF6A@users.noreply.github.com>
2023-01-27 12:23:42 -0500

    prueba

--- Proyecto_Final/src/proyecto_final/Proyecto_Final.java
+++ Proyecto_Final/src/proyecto_final/Proyecto_Final.java
@@ -15,6 +15,7 @@ public class Proyecto_Final {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        System.out.println("Hola");
     }
     
 }
