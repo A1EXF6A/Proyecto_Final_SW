49b3eb13803615bb48398e0a1b7ff5d5a39088c2
ajkarots <123658612+ajkarots@users.noreply.github.com>
2023-01-28 19:00:56 -0500

    Create reservas.java

---
+++ Proyecto_Final/src/proyecto_final/reservas.java
@@ -0,0 +1,38 @@
+package proyecto_final;
+
+
+public class Reservas {
+    int dia,mes,año;
+    String estado_de_reserva;
+    
+    public Reservas(int dia,int mes, int año,String estado_de_reserva ){
+        this.dia= dia;
+        this.mes= mes;
+        this.año= año;
+    }
+    
+     public int getDia() {
+        return dia;
+    }
+
+    public void setDia(int dia) {
+        this.dia = dia;
+    }
+
+    public int getMes() {
+        return mes;
+    }
+
+    public void setMes(int mes) {
+        this.mes = mes;
+    }
+
+    public String getEstadoreserva() {
+        return estado_de_reserva;
+    }
+
+    public void setEstadoreserva(String estado_de_reserva) {
+        this.estado_de_reserva = estado_de_reserva;
+    }
+   
+}
