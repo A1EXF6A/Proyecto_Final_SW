f1aaf60ab5a96bb686f7c02cd36f66ceb8d69960
Axios21 <123764929+Axios21@users.noreply.github.com>
2023-01-31 18:59:06 -0500

    menu de empleados

--- Proyecto_Final/src/proyecto_final/Menu_Hostal.java
+++ Proyecto_Final/src/proyecto_final/Menu_Hostal.java
@@ -9,18 +9,19 @@ import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Menu_Hostal {
+
     ArrayList<Personal_Servicios> ListaPersonal;
 
     public static void main(String[] args) throws IOException {
         int contador_archivos;
-        String nombre_archivoempleados="empleados.txt",nombre_archivohuespedes="huespedes.txt";
+        String nombre_archivoempleados = "empleados.txt", nombre_archivohuespedes = "huespedes.txt";
         File empleados = new File(nombre_archivoempleados);
         File huespedes = new File(nombre_archivohuespedes);
         FileWriter fwempleados = new FileWriter(empleados);
         PrintWriter Guardar_empleados = new PrintWriter(fwempleados);
         FileReader frempleados = new FileReader(empleados);
         Scanner leer_empleados = new Scanner(frempleados);
-        
+
         Scanner teclado = new Scanner(System.in);
         //Atributos
         int op, op1;
@@ -29,7 +30,7 @@ public class Menu_Hostal {
         //Codigo Menú
         do {
             System.out.printf("\n", "OPCIONES");
-            System.out.println("[1] Ingreso de datos del personal de servicios");
+            System.out.println("[1] Menú del Personal");
             System.out.println("[2] Ingreso-Salida de huespedes");
             System.out.println("[3] Datos de empleados y huespedes");
             System.out.println("[4] Datos Generales");
@@ -40,12 +41,43 @@ public class Menu_Hostal {
                 switch (op) {
                     case 1:
                         Personal_Servicios empleado = new Personal_Servicios("", "", "", "", "", "", "", "");
-                        empleado.IngresarDatos();
-                        Guardar_empleados.printf("%s,%s,%s,%s,%s,%s,%s,%s",empleado.cargo,empleado.cédula,empleado.nombre,empleado.apellido,
-                                empleado.direccion,empleado.telefono,empleado.hora_entrada,empleado.hora_salida);
-                        ListaPersonal.add(new Personal_Servicios(empleado.cargo,empleado.cédula,empleado.nombre,empleado.apellido,
-                                empleado.direccion,empleado.telefono,empleado.hora_entrada,empleado.hora_salida));
-                        Guardar_empleados.close();
+                        int op3;
+                        boolean salir = false;
+                        do {
+                            try {
+                                System.out.println("Que accion desea realizar: ");
+                                System.out.println("[1] Ingresar un nuevo empleado");
+                                System.out.println("[2] Ingresar a trabajar");
+                                System.out.println("[3] Salir de trabajar");
+                                System.out.println("[4] Regresar");
+                                op3 = Integer.valueOf(teclado.next());
+                                switch (op3) {
+                                    case 1:
+                                        empleado.IngresarDatos();
+                                        Guardar_empleados.printf("%s,%s,%s,%s,%s,%s,%s,%s", empleado.cargo, empleado.cédula, empleado.nombre, empleado.apellido,
+                                                empleado.direccion, empleado.telefono, empleado.hora_entrada, empleado.hora_salida);
+                                        ListaPersonal.add(new Personal_Servicios(empleado.cargo, empleado.cédula, empleado.nombre, empleado.apellido,
+                                                empleado.direccion, empleado.telefono, empleado.hora_entrada, empleado.hora_salida));
+                                        Guardar_empleados.close();
+                                        break;
+                                    case 2:
+                                        empleado.Entrar();
+                                        break;
+                                    case 3:
+                                        empleado.salir();
+                                        break;
+                                    case 4:
+                                        salir = true;
+                                        break;
+                                    default:
+                                        System.out.println("Ingresar solo numeros dentro del menu");
+                                        break;
+                                }
+                            } catch (NumberFormatException e) {
+                                System.out.println("Ingresar solo numeros");
+                            }
+                        } while (salir != true);
+
 //                        System.out.printf("|%-17s|%-17s|%-17s|%-17s|%-17s|%-17s|%-17s|%-17s|\n", "CARGO", "CEDULA", "NOMBRE", "APELLIDO", "DIRECCION", "TELEFONO", "HORA DE ENTRADA", "HORA DE SALIDA");
 //                        for (int i = 0; i < ListaPersonal.size(); i++) {
 //                            System.out.printf("|%-17s|%-17s|%-17s|%-17s|%-17s|%-17s|%-17s|%-17s|\n", ListaPersonal.get(i).cargo,
@@ -67,7 +99,7 @@ public class Menu_Hostal {
                                     op2 = teclado.next();
                                     if (op2.equalsIgnoreCase("Sí")) {
                                         huesped.Entrada();
-                                        ArrayList<Huespedes> ListaHuespedes=new ArrayList();
+                                        ArrayList<Huespedes> ListaHuespedes = new ArrayList();
                                         ListaHuespedes.add(huesped);
                                         //reservacion
                                     } else if (op2.equalsIgnoreCase("no")) {
@@ -80,7 +112,7 @@ public class Menu_Hostal {
                             case 2:
                                 System.out.println("Desea confirmar hospedaje");
                                 //Guardar en archivo
-                                
+
                                 break;
                             case 3:
                                 System.out.println("Desea salir del hotel");
@@ -93,17 +125,16 @@ public class Menu_Hostal {
 
                         break;
                     case 3:
-                       for (int i = 0; i < ListaPersonal.size(); i++) {
-                           
-                           System.out.printf("|%-17s|%-17s|%-17s|%-17s|%-17s|%-17s|%-17s|%-17s|\n", "CARGO", "CEDULA", "NOMBRE", "APELLIDO",
-                                   "DIRECCION", "TELEFONO", "HORA DE ENTRADA", "HORA DE SALIDA");
+                        for (int i = 0; i < ListaPersonal.size(); i++) {
+
+                            System.out.printf("|%-17s|%-17s|%-17s|%-17s|%-17s|%-17s|%-17s|%-17s|\n", "CARGO", "CEDULA", "NOMBRE", "APELLIDO",
+                                    "DIRECCION", "TELEFONO", "HORA DE ENTRADA", "HORA DE SALIDA");
                             System.out.printf("|%-17s|%-17s|%-17s|%-17s|%-17s|%-17s|%-17s|%-17s|\n", ListaPersonal.get(i).cargo,
                                     ListaPersonal.get(i).cédula, ListaPersonal.get(i).nombre, ListaPersonal.get(i).apellido,
                                     ListaPersonal.get(i).direccion, ListaPersonal.get(i).telefono, ListaPersonal.get(i).hora_entrada, ListaPersonal.get(i).hora_salida);
                         }
                         break;
-                        
-                        
+
                     case 4:
 
                         break;
